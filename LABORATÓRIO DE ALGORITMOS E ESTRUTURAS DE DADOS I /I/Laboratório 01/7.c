c30f48adfa5dc1a6aef697a9aa516da8ce3fd4c6
eltonlima01 <eltoncvlima@gmail.com>
2025-02-27 21:12:31 -0300

    commit

--- LABORATÓRIO DE ALGORITMOS E ESTRUTURAS DE DADOS I /I/Laboratório 01/7.c
+++ LABORATÓRIO DE ALGORITMOS E ESTRUTURAS DE DADOS I /I/Laboratório 01/7.c
@@ -1,3 +1,13 @@
+/*
+7º Desenvolva uma função que receba como parâmetro os ponteiros de
+dois vetores de 5 posições. O procedimento deverá imprimir na tela os
+valores contidos nos dois vetores de forma crescente (Utilize ponteiros).
+Exemplo:
+Vetor 1 = 2, 5, 9, 8, 3
+Vetor 2 = 7, 4, 1, 10, 6
+Saída: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+*/
+
 #include<stdio.h>
 
 void f(int *pA, int *pZ);
