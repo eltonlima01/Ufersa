3231005b5486cb17f4c2c9dbc6510b7c8103f03f
eltonlima01 <eltoncvlima@gmail.com>
2025-03-23 16:31:08 -0300

    .

---
+++ LABORATÓRIO DE ALGORITMOS E ESTRUTURAS DE DADOS I /Laboratório 05/2d.c
@@ -0,0 +1,15 @@
+/*
+Implemente os dois algoritmos de busca vistos em sala(Sequencial e Binária)
+e em seguida compare seus tempos de busca nos seguintes cenários:
+d) Buscando uma palavra em um texto
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+
+
+    return 0;
+}
