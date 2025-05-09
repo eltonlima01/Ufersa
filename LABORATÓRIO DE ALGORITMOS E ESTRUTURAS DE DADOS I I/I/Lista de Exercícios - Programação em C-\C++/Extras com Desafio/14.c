083692e3aadc27b7b1ad4a75e939cb9694738342
eltonlima01 <eltoncvlima@gmail.com>
2025-05-09 11:24:59 -0300

    Inserção da questão 14 (extra)

---
+++ LABORATÓRIO DE ALGORITMOS E ESTRUTURAS DE DADOS I I/I/Lista de Exercícios - Programação em C-\C++/Extras com Desafio/14.c
@@ -0,0 +1,34 @@
+// Escreva uma função para criptografar uma mensagem usando a Cifra de César (deslocamento de letras).
+
+#include <stdio.h>
+
+int main ()
+{
+    char string [100], ch = 'A';
+    int stringLenght;
+
+    puts ("[CIFRA DE CÉSAR]\nDigite a mensagem a ser criptografada:");
+
+    scanf ("%[^\n]", string);
+
+    for (int i = 0; i < (sizeof (string) / sizeof (char)); i++)
+    {
+        if (string [i] == '\0')
+        {
+            stringLenght = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < stringLenght; i++)
+    {
+        if (!(string [i] == ' '))
+        {
+            string [i] += 3;
+        }
+    }
+
+    printf ("Mensagem criptografada: %s\n", string);
+
+    return 0;
+}
